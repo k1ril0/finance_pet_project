6dedb9b24216702fb9b80f4693efed9b76c96cf3
k1rol <kirilka111007@gmail.com>
2025-07-07 19:57:39 +0200

    first working build of project

---
+++ finance/app/App.java
@@ -0,0 +1,28 @@
+package finance.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+   
+
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+     Parent root = FXMLLoader.load(getClass().getResource("/resource/LogInStage.fxml"));
+     Scene scene = new Scene(root,600,400);
+     primaryStage.setScene(scene);
+     primaryStage.setTitle("Finance App Demo");
+     primaryStage.show();
+    }
+    
+}
