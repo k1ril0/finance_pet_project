37ad7b498d87b0d729e6e7ff21a4430e0e0644d5
k1rol <kirilka111007@gmail.com>
2025-07-11 21:31:01 +0200

    a littel bit better version ig

--- finance/app/DataBaseController.java
+++ finance/app/DataBaseController.java
@@ -5,30 +5,19 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class DataBaseController {
-    String name = "root";
+            String name = "root";
             String password = "1234567";
             //Name pf your database or host where you runing this project
             String url = "jdbc:mysql://localhost:3306/name_OF_DataBase";
-//not sure if use here boid or just return connection as varieble 
-    public void getConnection(){
-        try{
-            // String name = "root";
-            // String password = "1234567";
-            // //Name pf your database or host where you runing this project
-            // String url = "jdbc:mysql://localhost:3306/name_OF_DataBase";
-            Connection mainConnection = DriverManager.getConnection(url,name,password);
-            System.out.println("connection has been creatd");
-        }catch(Exception e ){
-            e.printStackTrace();
-        }
-
-    }
-    public void Add(){
+            
+    public void Add(String USERNAME,String PASSWORD){
        try{
         Connection AddConnection = getTestConnection(name,password,url);
            if(AddConnection != null){
-             String SQL_ADD_COMMAND = "inser into you_dataBase() values (?,?)";
+             String SQL_ADD_COMMAND = "inser into 'table_name' (name,password) values (?,?)";
              PreparedStatement ADD_STATMENT = AddConnection.prepareStatement(SQL_ADD_COMMAND);
+             ADD_STATMENT.setString(1, USERNAME);
+             ADD_STATMENT.setString(2, PASSWORD);
              int rowsAffected = ADD_STATMENT.executeUpdate();
              if(rowsAffected > 0){
                System.out.println("Rowws affected : " + rowsAffected);
