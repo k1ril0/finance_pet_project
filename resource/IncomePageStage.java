8588ced200fb95419f149da03e092c3187fb6ad2
k1rol <kirilka111007@gmail.com>
2025-07-20 18:17:29 +0200

    added pie chart not funtional not fully in proces

--- resource/IncomePageStage.java
+++ resource/IncomePageStage.java
@@ -1,9 +1,13 @@
 package resource;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -11,10 +15,16 @@ public class IncomePageStage {
     
     @FXML
     private Button BackToMainStage;
+    @FXML
+    public PieChart pie_chart_of_spendings;
+    @FXML
+    private Button ThisMoth;
+
 
 
     @FXML
     private void GoBackToMainStage(){
+      // PieChart chart = IncomeChart();
         BackToMainStage.setOnAction(event ->{
               try{
                Parent root = FXMLLoader.load(getClass().getResource("/resource/MainStage.fxml"));
@@ -27,5 +37,32 @@ public class IncomePageStage {
 
               }
         });
+    }
+    @FXML
+    private void SpendingsOfThisMonth(){
+      Parent root;
+      PieChart thisMothChart =  IncomeChart();
+      try{
+         Stage stage = (Stage) ThisMoth.getScene().getWindow();
+         Scene scene = new Scene(thisMothChart,600,400);
+         stage.setScene(scene);
+         stage.setTitle("This month Chart");
+         stage.show();
+      }catch(Exception e){
+        e.printStackTrace();
+      }
+    }
+    private PieChart IncomeChart(){
+      ObservableList<PieChart.Data> listOfDataMoney = 
+      FXCollections.observableArrayList();
+
+      PieChart chart = new PieChart();
+
+      listOfDataMoney.add(new PieChart.Data("Appels",50));
+      listOfDataMoney.add(new PieChart.Data("Oranges", 40));
+
+      chart.setData(listOfDataMoney);
+
+      return chart; 
     }
 }
